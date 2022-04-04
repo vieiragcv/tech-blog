236fc00d8c22e8a0d26729b008e4fff6f9832ba8
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-04-03 22:43:29 -0400

    added Post model and routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,137 @@
+/*---------------------------------------------------------------
+-                         POST-ROUTES
+---------------------------------------------------------------*/
+
+const sequelize = require('../../config/connection');
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+/*---------------------------------------------------------------
+-                         GET /api/posts
+---------------------------------------------------------------*/
+
+/*------------------------   GET ALL     ----------------------*/
+
+router.get('/', (req, res) => {
+  console.log('======================');
+  Post.findAll({
+    order: [['created_at', 'DESC']],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+/*------------------------   GET 1     -----------------------*/
+
+router.get('/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+/*---------------------------------------------------------------
+-                         CREATE POSTS /api/posts
+---------------------------------------------------------------*/
+
+router.post('/', (req, res) => {
+  Post.create({
+    title: req.body.title,
+    post_url: req.body.post_url,
+    user_id: req.body.user_id
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+/*---------------------------------------------------------------
+-                         UPDATE POSTS /api/posts
+---------------------------------------------------------------*/
+
+router.put('/:id', (req, res) => {
+  Post.update(
+    {
+      title: req.body.title
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+/*---------------------------------------------------------------
+-                         DELETE POSTS /api/posts
+---------------------------------------------------------------*/
+
+router.delete('/:id', (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -2,3 +2,16 @@
 -                         MODULES INDEX
 ---------------------------------------------------------------*/
 
+const User = require('./User');
+const Post = require('./Post');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+
+module.exports = { User };
